fe689f210a6db2ca2617e63370eecdffdc4b4cd9
Sahil Muthoo <sahil.muthoo@gmail.com>
2013-01-01 15:49:47 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleRailsAppRspecFeatures::Application.config.secret_token = '96e448cb407f4b732ab0226fd8f7cfac205b833da6317c41c122ed18bb976160b7ea790a7b062946e82bfbe1f17368fb1bc60495b108717edd169604ab63e245'
---
+++ spec/features/posts_spec.rb
@@ -0,0 +1,6 @@
+describe "posts" do
+  it "should have the right content" do
+    visit posts_path
+    page.should have_content("Listing posts")
+  end
+end
